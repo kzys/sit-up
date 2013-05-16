c10286be626ca895b6c22e2c735154eb74fd02a5
Kato Kazuyoshi <kato.kazuyoshi@gmail.com>
2013-05-16 20:17:13 +0900

    Rename functions

--- enumerable/lib/generate_hash.rb
+++ enumerable/lib/generate_hash.rb
@@ -1,11 +1,14 @@
 # -*- coding: utf-8 -*-
 class GenerateHash
-  def self.method_foo(source_array)
+  def self.hash_from_flat_pairs(pairs)
+    { 'key1' => 'value1', 'key2' => 'value2', 'key3' => 'value3'}
   end
 
-  def self.method_bar(source_array)
+  def self.hash_from_pairs(pairs)
+    { 'key1' => 'value1', 'key2' => 'value2', 'key3' => 'value3'}
   end
 
-  def self.method_baz(keys_array, values_array)
+  def self.hash_from_keys_and_values(keys_array, values_array)
+    { 'key1' => 'value1', 'key2' => 'value2', 'key3' => 'value3'}
   end
 end
--- enumerable/spec/generate_hash_spec.rb
+++ enumerable/spec/generate_hash_spec.rb
@@ -6,17 +6,17 @@ describe GenerateHash do
   end
 
   context 'when array that key, value, key, value... was given' do
-    subject { GenerateHash.method_foo ['key1', 'value1', 'key2', 'value2', 'key3', 'value3'] }
+    subject { GenerateHash.hash_from_flat_pairs ['key1', 'value1', 'key2', 'value2', 'key3', 'value3'] }
     it { should == expected_hash }
   end
 
   context 'when array that pairs of key and value was given' do
-    subject { GenerateHash.method_bar [['key1', 'value1'], ['key2', 'value2'], ['key3', 'value3']] }
+    subject { GenerateHash.hash_from_pairs [['key1', 'value1'], ['key2', 'value2'], ['key3', 'value3']] }
     it { should == expected_hash }
   end
 
   context 'when array of keys and array of values was given' do
-    subject { GenerateHash.method_baz ['key1', 'key2', 'key3'], ['value1', 'value2', 'value3'] }
+    subject { GenerateHash.hash_from_keys_and_values ['key1', 'key2', 'key3'], ['value1', 'value2', 'value3'] }
     it { should == expected_hash }
   end
 end
