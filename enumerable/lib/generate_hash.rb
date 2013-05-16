8ab8f60a413c1bb9e1bcae7aa89bac2a237b92e8
Kato Kazuyoshi <kato.kazuyoshi@gmail.com>
2013-05-16 20:20:05 +0900

    Implement actual methods
    However I copied most from http://doc.ruby-lang.org/ja/1.9.3/class/Hash.html.

--- enumerable/lib/generate_hash.rb
+++ enumerable/lib/generate_hash.rb
@@ -1,14 +1,14 @@
 # -*- coding: utf-8 -*-
 class GenerateHash
   def self.hash_from_flat_pairs(pairs)
-    { 'key1' => 'value1', 'key2' => 'value2', 'key3' => 'value3'}
+    Hash[*pairs]
   end
 
   def self.hash_from_pairs(pairs)
-    { 'key1' => 'value1', 'key2' => 'value2', 'key3' => 'value3'}
+    Hash[*pairs.flatten(1)]
   end
 
-  def self.hash_from_keys_and_values(keys_array, values_array)
-    { 'key1' => 'value1', 'key2' => 'value2', 'key3' => 'value3'}
+  def self.hash_from_keys_and_values(keys, values)
+    Hash[keys.zip(values)]
   end
 end
